3cc7d7453df35f971c026f39e8fc6093a3a55a46
Yohann <yoyo24du26@gmail.com>
2026-02-03 17:50:40 +0100

    feat(Header): fixing some details

--- frontend/components/Header/Header.tsx
+++ frontend/components/Header/Header.tsx
@@ -7,19 +7,19 @@ import { EarthIcon, Github, PlusIcon } from "lucide-react"
 export default function Header() {
     return (
         <header className="flex justify-between items-center py-4 px-10 border-b-1">
-            <h1>Gougle doc</h1>
+            <Link href="/" title="Home page">Gougle doc</Link>
             <div className="flex items-end gap-2">
-                <Link href='docs' className="flex items-center gap-2">
-                    <Button className="bg-cyan-300 text-cyan-800 hover:bg-cyan-500 hover:text-white">
-                        <PlusIcon /> New Doc
+                <Link href='docs' className="flex items-center gap-2" title="New document">
+                    <Button className="bg-cyan-300 text-cyan-800 hover:bg-cyan-500 hover:text-white rounded-full">
+                        <PlusIcon /> New document
                     </Button>
                 </Link>
                 
                 <ButtonGroup>
-                    <Button size="icon" variant="outline">
+                    <Button size="icon" variant="outline" className="rounded-s-full" title="My GitHub">
                         <Link href='https://github.com/yohannimation' target="_blank"><Github /></Link>
                     </Button>
-                    <Button size="icon" variant="outline">
+                    <Button size="icon" variant="outline" className="rounded-e-full" title="My Portfolio">
                         <Link href='https://yohannimation.fr' target="_blank"><EarthIcon /></Link>
                     </Button>
                 </ButtonGroup>
