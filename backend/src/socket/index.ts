ea051f4d265c72be7e0a957cce3940ff9169c381
Yohann <yoyo24du26@gmail.com>
2026-02-10 13:47:46 +0100

    feat(backend/api): creating `websocket handler`

---
+++ backend/src/socket/index.ts
@@ -0,0 +1,27 @@
+import { Server as HttpServer } from 'http'
+import { Server as SocketServer } from 'socket.io'
+import { config } from '../config/env'
+import { documentHandler } from './handlers/document.handler'
+
+export function setupSocketHandlers(httpServer: HttpServer) {
+    const io = new SocketServer(httpServer, {
+        cors: {
+        origin: config.frontendUrl,
+        methods: ['GET', 'POST'],
+        credentials: true,
+        },
+    })
+
+    io.on('connection', (socket) => {
+        console.log(`✅ User connected: ${socket.id}`)
+
+        // Document Handler
+        documentHandler(io, socket)
+
+        socket.on('disconnect', () => {
+        console.log(`❌ User disconnected: ${socket.id}`)
+        })
+    })
+
+    return io
+}
