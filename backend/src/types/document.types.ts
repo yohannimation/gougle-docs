a90e55f2a6ab3f734c884e17dbbf52f8696d8a18
Yohann <yoyo24du26@gmail.com>
2026-02-10 13:48:01 +0100

    feat(backend/api): creating `document interface`

---
+++ backend/src/types/document.types.ts
@@ -0,0 +1,20 @@
+import { Document } from '@prisma/client'
+
+export type DocumentCreateInput = {
+    name: string
+    isEditable?: boolean
+    content?: any
+}
+
+export type DocumentUpdateInput = {
+    name?: string
+    isEditable?: boolean
+    content?: any
+}
+
+export type DocumentResponse = Document
+
+export type DocumentListResponse = {
+    documents: Document[]
+    total: number
+}
