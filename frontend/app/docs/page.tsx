97ef2da60ed3c88c5d2577f62eced3987b8162fe
Yohann <yoyo24du26@gmail.com>
2026-02-10 20:21:51 +0100

    refactor(frontend/documents-page): refactoring code

--- frontend/app/docs/page.tsx
+++ frontend/app/docs/page.tsx
@@ -12,23 +12,20 @@ export default function Docs() {
         <li key={i}><DocumentCardSkeleton /></li>
     ));
 
-    if (isLoading) {
-        return (<>
-            <h1>Docs</h1>
-            <ul className="mt-3 grid sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3">
-                { skeletons }
-            </ul>
-        </>)
-    }
-
     return (<>
         <h1>Docs</h1>
         <ul className="mt-3 grid sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3">
-            {documents.map(document => (
-                <li key={document.id}>
-                    <DocumentCard document={document} deleteDocument={deleteDocument} />
-                </li>
-            ))}
+            {
+                isLoading ? (
+                    skeletons
+                ) : (
+                    documents.map(document => (
+                        <li key={document.id}>
+                            <DocumentCard document={document} deleteDocument={deleteDocument} />
+                        </li>
+                    ))
+                )
+            }
         </ul>
     </>)
 }
