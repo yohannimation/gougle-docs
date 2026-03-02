fff8f0370e9a825f4442b15d2d58bf6cb515f433
Yohann <yoyo24du26@gmail.com>
2026-03-02 17:05:09 +0100

    feat(frontend/document-page): adding an empty document catcher

--- frontend/components/DocumentsList/DocumentsList.tsx
+++ frontend/components/DocumentsList/DocumentsList.tsx
@@ -1,5 +1,14 @@
 import DocumentCard from '@/components/DocumentCard/DocumentCard';
 import DocumentCardSkeleton from '@/components/DocumentCardSkeleton/DocumentCardSkeleton';
+import {
+    Empty,
+    EmptyDescription,
+    EmptyHeader,
+    EmptyMedia,
+    EmptyTitle,
+} from '@/components/ui/empty';
+
+import { FileX } from 'lucide-react';
 
 import { Document } from '@/lib/api/documents';
 
@@ -19,6 +28,24 @@ export default function DocumentsList({
             <DocumentCardSkeleton />
         </li>
     ));
+
+    if (documents.length == 0 && !isLoading) {
+        return (
+            <Empty>
+                <EmptyHeader>
+                    <EmptyMedia variant="icon">
+                        <FileX />
+                    </EmptyMedia>
+                    <EmptyTitle>No documents yet</EmptyTitle>
+                    <EmptyDescription>
+                        You haven't created any documents.
+                        <br />
+                        Get started by creating your first file.
+                    </EmptyDescription>
+                </EmptyHeader>
+            </Empty>
+        );
+    }
 
     return (
         <ul className="grid sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3">
